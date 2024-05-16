7b405e905bacebf9753adf13b1a411b6671498e9
zTomz <tomvogel0@gmail.com>
2024-05-16 13:29:36 +0200

    Created example

---
+++ example/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ionicons/flutter_ionicons.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Icon(Ionicons.home),
+        ),
+      ),
+    );
+  }
+}
